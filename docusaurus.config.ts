dd07f80e935ec9b555759516024d59904c27dfce
Cattyman <88530324+cattyman919@users.noreply.github.com>
2024-11-27 20:49:52 +0700

    update content

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
+require("dotenv").config();
+
 const config: Config = {
   title: "DevSen",
   tagline:
